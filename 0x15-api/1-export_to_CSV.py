fe50531cfe5fd6455e6a6d4419f8b31815fa9e73
James <jmartinez21734@stu.nuc.edu>
2021-05-04 03:00:35 +0000

    fixing pep8

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,6 +3,7 @@
 Module that use a REST API, for a given employee ID, returns information about
 his/her TODO list progress
 """
+
 import requests
 import csv
 from sys import argv
@@ -14,7 +15,16 @@ if __name__ == '__main__':
     todo = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
                         format(employee_id)).json()
 
-    with open('{}.csv'.format(employee_id), "w") as csv_f:
+    task_done = []
+    for task in todo:
+        if task.get('completed') is True:
+            task_done.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(task_done), len(todo)))
+    for done in task_done:
+        print("\t {}".format(done))
+
+    with open('{}.csv'.format(argv[1]), "w") as csv_f:
         data = csv.writer(csv_f, quoting=csv.QUOTE_ALL)
         for task in todo:
             data.writerow(
