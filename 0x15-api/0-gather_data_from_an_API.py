1c9217c26cb55291fc81e4b086e4bead253865a6
James <jmartinez21734@stu.nuc.edu>
2021-05-04 02:25:53 +0000

    addinf file 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,13 +9,14 @@ from sys import argv
 if __name__ == '__main__':
     employee_id = int(argv[1])
     user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
-                        format(employee_id), verify=False).json()
+                        format(employee_id)).json()
     todo = requests.get('https://jsonplaceholder.typicode.com/todos?userID={}'.
-                        format(employee_id), verify=False).json()
+                        format(employee_id)).json()
     task_done = []
     for task in todo:
         if task.get('completed') is True:
             task_done.append(task.get('title'))
     print("Employee {} is done with tasks({}/{}):".
           format(user.get('name'), len(task_done), len(todo)))
-    print("\n".join("\t {}".format(task) for task in task_done))
+    for done in task_done:
+        print("\t {}".format(done))
