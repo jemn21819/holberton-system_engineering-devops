5cace41dc816d93e55e57b9e86c3397ca6819526
James <jmartinez21734@stu.nuc.edu>
2021-05-04 02:45:15 +0000

    addinf task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module that use a REST API, for a given employee ID, returns information about
+his/her TODO list progress
+"""
+import requests, csv
+from sys import argv
+
+if __name__ == '__main__':
+    employee_id = int(argv[1])
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                        format(employee_id)).json()
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+                        format(employee_id)).json()
+
+    task_done = []
+    for task in todo:
+        if task.get('completed') is True:
+            task_done.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(task_done), len(todo)))
+    for done in task_done:
+        print("\t {}".format(done))
+
+    with open('{}.csv'.format(employee_id), "w") as csv_f:
+        data = csv.writer(csv_f, quoting=csv.QUOTE_ALL)
+        for task in todo:
+            data.writerow(
+                [employee_id,
+                    user.get('username'),
+                    task.get('completed'),
+                    task.get('title')])
