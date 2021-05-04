fb89209ab3ead2025bdd75c2860dcb74e7117f0d
James <jmartinez21734@stu.nuc.edu>
2021-05-04 03:14:16 +0000

    adding task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Module that use a REST API, for a given employee ID, returns information about
+his/her TODO list progress
+"""
+import csv
+import json
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    employee_id = int(argv[1])
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                        format(employee_id)).json()
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+                        format(employee_id)).json()
+
+    task_done = []
+    for task in todo:
+        if task.get('completed') is True:
+            task_done.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(task_done), len(todo)))
+    for done in task_done:
+        print("\t {}".format(done))
+
+    with open('{}.csv'.format(employee_id), "w") as csv_f:
+        data = csv.writer(csv_f, quoting=csv.QUOTE_ALL)
+        for task in todo:
+            data.writerow(
+                [employee_id,
+                 user.get('username'),
+                 task.get('completed'),
+                 task.get('title')])
+
+    for task in todo:
+        file_format = {
+                'task': task.get('title'),
+                'completed': task.get('completed'),
+                'username': user.get('username')
+                }
+        task_done.append(file_format)
+
+    json_task = {}
+    json_task[employee_id] = task_done
+    with open('{}.json'.format(employee_id), "w") as json_f:
+        json.dump(json_task, json_f)
