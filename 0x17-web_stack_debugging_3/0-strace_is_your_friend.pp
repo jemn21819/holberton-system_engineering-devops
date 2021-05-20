9a6b4caca9d8d8a28bcbb9cc8a7055feba04d615
James <jmartinez21734@stu.nuc.edu>
2021-05-20 01:44:57 +0000

    fixin puppet lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# find out why Apache is returning a 500 error
 # fix it and then automate it using Puppet
 exec {'fix php ext.':
   command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
-    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 }
