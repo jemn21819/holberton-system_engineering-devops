7412810a1f61f4afe24dd2c98547d133e345a556
James <jmartinez21734@stu.nuc.edu>
2021-05-20 01:41:29 +0000

    initial commit, readme file and task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# find out why Apache is returning a 500 error
+# fix it and then automate it using Puppet
+exec {'fix php ext.':
+  command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
+    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+}
